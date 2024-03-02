ad35e68273951c66c512ee1d844806dff70399b8
X-I-A <develop@x-i-a.com>
2024-03-02 08:23:39 +0100

    feat: creating packages.yaml

--- xia_framework/application.py
+++ xia_framework/application.py
@@ -1,5 +1,22 @@
+import importlib
+import yaml
 from xia_framework.framework import Framework
 
 
 class Application(Framework):
-    pass
+    def create(self, module_name: str):
+        """Create a module
+
+        Args:
+            module_name (str):
+        """
+        with open(self.module_yaml, 'r') as file:
+            module_dict = yaml.safe_load(file) or {}
+        if module_name not in module_dict:
+            raise ValueError(f"Module {module_name} is not configured")
+        module_config = module_dict[module_name]
+        module_obj = importlib.import_module(module_config["package"].replace("-", "_"))
+        module_class = getattr(module_obj, module_config["class"])
+        module_instance = module_class()
+        init_config = module_config.get("events", {}).get("init", {}) or {}
+        module_instance.initialize(**init_config)
--- xia_framework/framework.py
+++ xia_framework/framework.py
@@ -8,6 +8,7 @@ class Framework:
         self.config_dir = config_dir
         self.landscape_yaml = os.path.sep.join([self.config_dir, "landscape.yaml"])
         self.module_yaml = os.path.sep.join([self.config_dir, "modules.yaml"])
+        self.package_yaml = os.path.sep.join([self.config_dir, "packages.yaml"])
 
     def get_needed_packages(self, ignore_existed: bool = False) -> dict:
         """Get needed packages in requirements.txt form
@@ -16,23 +17,19 @@ class Framework:
             ignore_existed (bool): Do not check local packages
 
         """
-        with open(self.module_yaml, 'r') as file:
-            module_dict = yaml.safe_load(file) or {}
+        with open(self.package_yaml, 'r') as file:
+            package_dict = yaml.safe_load(file) or {}
 
-        package_dict = {}
-        for module_name, module_config in module_dict.items():
-            package_name = module_config["package"]
+        package_addresses = {}
+        for package_name, package_config in package_dict.items():
             package_dir = package_name.replace("-", "_")
-            if package_name in package_dict:
-                # Case 1: Package already configured, nothing to do
-                continue
-            elif not ignore_existed and os.path.exists(f"./{package_dir}"):
+            if not ignore_existed and os.path.exists(f"./{package_dir}"):
                 print(f"Found local package {package_name}: ./{package_dir}")
             elif not ignore_existed and os.path.exists(f"../{package_name}"):
                 print(f"Found local package {package_name}: ../{package_name}")
             else:
-                package_version = module_config.get("version", None)
-                git_https_url = module_config.get("git_https", None)
+                package_version = package_config.get("version", None)
+                git_https_url = package_config.get("git_https", None)
                 if git_https_url:
                     if package_version:
                         package_address = f"git+https://{git_https_url}@{package_version}#egg={package_name}"
@@ -40,8 +37,8 @@ class Framework:
                         package_address = f"git+https://{git_https_url}#egg={package_name}"
                 else:
                     package_address = f"{package_name}=={package_version}" if package_version else package_name
-                package_dict[package_name] = package_address
-        return package_dict
+                package_addresses[package_name] = package_address
+        return package_addresses
 
     def terraform_init(self, env: str):
         with open(self.landscape_yaml, 'r') as file:
