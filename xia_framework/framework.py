e53022053093f76c8a55b31ffe340bb4d4c7d437
X-I-A <develop@x-i-a.com>
2024-08-27 21:51:27 +0200

    feat: activate module support

--- xia_framework/framework.py
+++ xia_framework/framework.py
@@ -37,26 +37,6 @@ class Framework:
             version = None
         return package_name, version, module_name
 
-    def load_module(self, module_uri: str) -> dict:
-        """Load Module
-
-        Args:
-            module_uri (str): Module name as format <package_name>@<version>/<module_name>
-
-        Returns:
-            module_dict (dict): module dict after the load
-        """
-        package_name, version, module_name = self._parse_module_uri(module_uri=module_uri)
-        package_address = self.get_package_address(package_name=package_name, package_version=version)
-        with open(self.module_yaml, 'r') as module_file:
-            module_dict = self.yaml.load(module_file) or {}
-        new_module = True if module_name not in module_dict else False
-        if new_module:
-            if package_address:
-                # Installation of package
-                subprocess.run(['pip', 'install', package_address], check=True)
-        return module_dict
-
     def init_module(self, module_uri: str):
         """initialize a module
 
@@ -72,7 +52,7 @@ class Framework:
             if package_address:
                 # Installation of package
                 subprocess.run(['pip', 'install', package_address], check=True)
-        module_dict[module_name] = {"package": package_name, "class": None, "events": {"deploy": None}}
+        module_dict[module_name] = {"package": package_name, "events": {"deploy": None}}
         module_config = module_dict[module_name]
         module_obj = importlib.import_module(module_config["package"].replace("-", "_"))
         module_class_name = getattr(module_obj, "modules", {}).get(module_name)
@@ -91,7 +71,6 @@ class Framework:
                     package_dict["packages"][package_name] = None
                 with open(self.package_yaml, 'w') as package_file:
                     self.yaml.dump(package_dict, package_file)
-            module_dict[module_name]["class"] = module_class_name
             with open(self.module_yaml, 'w') as module_file:
                 self.yaml.dump(module_dict, module_file)
 
@@ -112,7 +91,7 @@ class Framework:
             if package_address:
                 # Installation of package
                 subprocess.run(['pip', 'install', package_address], check=True)
-        module_dict[module_name] = {"package": package_name, "class": None, "events": {"activate": None}}
+        module_dict[module_name] = {"package": package_name, "events": {"activate": None}}
         module_config = module_dict[module_name]
         if activate_scope:
             module_config["activate_scope"] = activate_scope
