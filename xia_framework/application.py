1f8e0226aa9a7c3671aabad2824b4827f75ada4a
X-I-A <develop@x-i-a.com>
2024-08-28 21:47:26 +0200

    feat: cicd default rework

--- xia_framework/application.py
+++ xia_framework/application.py
@@ -6,43 +6,6 @@ from xia_framework.framework import Framework
 
 
 class Application(Framework):
-    def create(self, module_uri: str):
-        """Initialize a module
-
-        Args:
-            module_uri (str): Module name as format <package_name>@<version>/<module_name>
-        """
-        package_name, version, module_name = self._parse_module_uri(module_uri=module_uri)
-        package_address = self.get_package_address(package_name=package_name, package_version=version)
-        with open(self.module_yaml, 'r') as module_file:
-            module_dict = self.yaml.load(module_file) or {}
-        new_module = True if module_name not in module_dict else False
-        if new_module:
-            if package_address:
-                # Installation of package
-                subprocess.run(['pip', 'install', package_address], check=True)
-        module_dict[module_name] = {"package": package_name, "class": None, "events": {"deploy": None}}
-        module_config = module_dict[module_name]
-        module_obj = importlib.import_module(module_config["package"].replace("-", "_"))
-        module_class_name = getattr(module_obj, "modules", {}).get(module_name)
-        module_class = getattr(module_obj, module_class_name)
-        module_instance = module_class()
-        init_config = module_config.get("events", {}).get("init", {}) or {}
-        module_instance.initialize(**init_config)
-        if new_module:
-            # All goes well, should be safe to save the modified module configuration
-            with open(self.package_yaml, 'r') as package_file:
-                package_dict = self.yaml.load(package_file) or {}
-            if package_name not in package_dict:
-                if version:
-                    package_dict["packages"][package_name] = {"version": version}
-                else:
-                    package_dict["packages"][package_name] = None
-                with open(self.package_yaml, 'w') as package_file:
-                    self.yaml.dump(package_dict, package_file)
-            with open(self.module_yaml, 'w') as module_file:
-                self.yaml.dump(module_dict, module_file)
-
     def terraform_get_state_file_prefix(self, env_name: str = None):
         with open(self.landscape_yaml, 'r') as file:
             landscape_dict = yaml.safe_load(file) or {}
