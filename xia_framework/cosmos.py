e8ee3fb92273c4fe63073a8d2a8bf1d79199b197
X-I-A <develop@x-i-a.com>
2024-09-01 19:35:55 +0200

    feat: Cosmos Configuration

--- xia_framework/cosmos.py
+++ xia_framework/cosmos.py
@@ -3,6 +3,7 @@ import argparse
 import subprocess
 import yaml
 from xia_framework.application import Application
+from xia_framework.singularity import GcpSingularity
 
 
 class Cosmos(Application):
@@ -12,55 +13,31 @@ class Cosmos(Application):
             "bigbang": {"cli": self.cli_bigbang, "run": self.cmd_bigbang},
             "activate-module": {"cli": self.cli_activate_module, "run": self.cmd_activate_module},
         })
+        self.topology_dict = {
+            "gcp": [GcpSingularity]
+        }
 
-    def bigbang(self, cosmos_name: str):
+    def bigbang(self):
         """Create the cosmos administration project
-
-        TODO:
-            * Activate Cloud Billing API during cosmos project creation
-            * Activate Cloud Resource Manager API (cloudresourcemanager.) during cosmos project creation
-            * Activate Identity and Access Management API (iam.) during cosmos project creation
-
-        Args:
-            cosmos_name (str): Cosmos Name
         """
-
         with open(self.landscape_yaml, 'r') as file:
             landscape_dict = yaml.safe_load(file) or {}
-        current_settings = landscape_dict.get("settings", {})
-        current_cosmos_name = current_settings.get("cosmos_name", "")
-        if not cosmos_name:
-            raise ValueError("Realm project must be provided")
-        if current_cosmos_name and current_cosmos_name != cosmos_name:
-            raise ValueError("Realm project doesn't match configured landscape.yaml")
-        get_billing_cmd = f"gcloud billing accounts list --filter='open=true' --format='value(ACCOUNT_ID)' --limit=1"
-        r = subprocess.run(get_billing_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-        current_settings["billing_account"] = r.stdout if "ERROR" not in r.stderr else None
-        print(current_settings["billing_account"])
-        check_project_cmd = f"gcloud projects list --filter='{cosmos_name}' --format='value(projectId)'"
-        r = subprocess.run(check_project_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-        if cosmos_name in r.stdout:
-            print(f"Realm Project {cosmos_name} already exists")
-        else:
-            create_proj_cmd = f"gcloud projects create {cosmos_name} --name='{cosmos_name}'"
-            r = subprocess.run(create_proj_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-            if "ERROR" not in r.stderr:
-                print(f"Realm Project {cosmos_name} create successfully")
-                current_settings["cosmos_name"] = cosmos_name
-                with open(self.landscape_yaml, 'w') as file:
-                    yaml.dump(landscape_dict, file, default_flow_style=False, sort_keys=False)
-            else:
-                print(r.stderr)
+
+        topology_type = landscape_dict.get("topology", {}).get("type", None)
+        if not topology_type:
+            raise ValueError(f"Please define the topology in config/landscape.yaml")
+        if topology_type not in self.topology_dict:
+            raise ValueError(f"Topology {topology_type} is not among {list(self.topology_dict)}")
+
+        for singularity in self.topology_dict["topology_name"]:
+            singularity.bigbang(**landscape_dict["topology"])
 
     def terraform_get_state_file_prefix(self, env_name: str = None):
         return f"_/terraform/state"
 
     @classmethod
     def cli_bigbang(cls, subparsers):
-        sub_parser = subparsers.add_parser('bigbang', help='Create Cosmos Singularity')
-        sub_parser.add_argument('-t', '--topology',
-                                type=str, help='Cosmos topology', default="github:x-i-a/xia-cosmos-template")
-        sub_parser.add_argument('-p', '--params', type=str, help='Bigbang Parameters')
+        subparsers.add_parser('bigbang', help='Create Cosmos Singularity')
 
     @classmethod
     def cli_activate_module(cls, subparsers):
@@ -84,7 +61,7 @@ class Cosmos(Application):
         sub_parser.add_argument('-y', '--auto-approve', type=str, help='Approve destroy automatically')
 
     def cmd_bigbang(self, args):
-        self.bigbang(cosmos_name=args.name)
+        self.bigbang()
 
     def cmd_activate_module(self, args):
         self.activate_module(module_uri=args.module_uri)
---
+++ xia_framework/singularity.py
@@ -0,0 +1,55 @@
+import subprocess
+
+
+class Singularity:
+    @classmethod
+    def bigbang(cls, **kwargs):
+        """Bigbang of different topology
+
+        Args:
+            **kwargs: Bigbang parameters
+        """
+
+
+class GcpSingularity:
+    @classmethod
+    def bigbang(cls, cosmos_project: str, cosmos_bucket_name: str, cosmos_bucket_region: str, **kwargs):
+        """GCP Bigbang. Handle project will be defined and Terraform will be saved in the given bucket
+
+        Args:
+            cosmos_project: cosmos project to be created
+            cosmos_bucket_name: state file of the cosmos to be saved in this bucket
+            cosmos_bucket_region: bucket should be located in this region
+
+        """
+        # get_billing_cmd = f"gcloud billing accounts list --filter='open=true' --format='value(ACCOUNT_ID)' --limit=1"
+        # r = subprocess.run(get_billing_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        # billing_account = r.stdout if "ERROR" not in r.stderr else None
+        # print(f"GCP Billing Account detected: {billing_account}")
+        # Step 1: Create project
+        check_project_cmd = f"gcloud projects list --filter='{cosmos_project}' --format='value(projectId)'"
+        r = subprocess.run(check_project_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        if cosmos_project in r.stdout:
+            raise ValueError(f"Cosmos Project {cosmos_project} already exists")
+        else:
+            create_proj_cmd = f"gcloud projects create {cosmos_project} --name='{cosmos_project}'"
+            r = subprocess.run(create_proj_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+            if "ERROR" not in r.stderr:
+                print(f"Cosmos Project {cosmos_project} created successfully")
+            else:
+                raise Exception(r.stderr)
+        # Step 2: Activate API services
+        for service in ["cloudresourcemanager", "iam", "cloudbilling", "storage"]:
+            enable_api_cmd = f"gcloud services enable {service}.googleapis.com"
+            r = subprocess.run(enable_api_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+            if "ERROR" not in r.stderr:
+                print(f"Service {service} enabled successfully in Cosmos Project {cosmos_project}")
+            else:
+                raise Exception(r.stderr)
+        # Step 3: Create Bucket for saving terraform state files
+        create_bucket_cmd = f"gcloud storage buckets create {cosmos_bucket_name} --location {cosmos_bucket_region}"
+        r = subprocess.run(create_bucket_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        if "ERROR" not in r.stderr:
+            print(f"Cosmos Bucket {cosmos_bucket_name} created successfully in {cosmos_bucket_region}")
+        else:
+            raise Exception(r.stderr)
