ab1e00ec3fb02e0798fb352dd0fcceb7cfb7f75b
X-I-A <develop@x-i-a.com>
2024-09-07 18:53:44 +0200

    feat: enrich bigbang

--- xia_framework/cosmos.py
+++ xia_framework/cosmos.py
@@ -58,6 +58,7 @@ class Cosmos(Application):
         landscape_replace_dict = {
             key + ":": f"  {key}: {value}\n" for key, value in current_settings.items()
         }
+        landscape_replace_dict["type:"] = f"  type: {topology_type}"
         self._config_replace(self.landscape_yaml, landscape_replace_dict)
 
         # Step 3: Bigbang
